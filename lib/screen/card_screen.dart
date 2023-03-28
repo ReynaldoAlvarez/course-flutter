3bea77c422b1f04b4a9e19813d18118ba218cd7e
Vidal Alvarez <vidal.r.a.777@gmail.com>
2023-03-28 09:52:47 -0400

    finish custom card

--- lib/screen/card_screen.dart
+++ lib/screen/card_screen.dart
@@ -1,3 +1,4 @@
+import "package:fl_components/widgets/card_bible2.dart";
 import "package:fl_components/widgets/widgets.dart";
 import "package:flutter/material.dart";
 
@@ -12,7 +13,24 @@ class CardScreen extends StatelessWidget {
       ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        children: const [CardBible()],
+        children: const [
+          CardBible(),
+          SizedBox(
+            height: 10,
+          ),
+          CardBible2(
+              imageUrl:
+                  'https://cdn.pixabay.com/photo/2015/03/30/20/33/heart-700141_1280.jpg',
+              name: null),
+          SizedBox(
+            height: 10,
+          ),
+          CardBible2(
+            imageUrl:
+                'https://recursoscristianosweb.com/wp-content/uploads/2018/04/Que-es-la-Gracia-de-Dios-Estudio-Biblico.webp',
+            name: 'Jesús',
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,3 @@
+import 'package:fl_components/widgets/card_bible2.dart';
+
 export 'package:fl_components/widgets/card_bible.dart';
